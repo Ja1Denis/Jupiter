a1346acd69293f0fab4629a0e74330ee8f1ec0ba
JaDenis <sdenis.vr@gmail.com>
2018-02-19 20:25:40 +0100

    Instaling latest npm and bower beacose gulp watch is not working

--- site/web/app/themes/jupiter-theme/template-slider.php
+++ site/web/app/themes/jupiter-theme/template-slider.php
@@ -8,12 +8,23 @@
        <div class="carousel-inner">
          <div class="carousel-item active">
            <img class="d-block w-100" src="https://cdn.pixabay.com/photo/2012/01/09/10/41/jupiter-11615_640.png" alt="First slide" >
-         </div>
+           <div class="carousel-caption d-none d-md-block">
+              <h5>Prvi</h5>
+              <p>Prva slika</p>
+           </div>
          <div class="carousel-item">
            <img class="d-block w-100 " src="https://cdn.pixabay.com/photo/2014/11/13/21/36/jupiter-529959_640.jpg" alt="Second slide">
+           <div class="carousel-caption d-none d-md-block">
+              <h5>Drugi</h5>
+              <p>Druga slika</p>
+           </div>
          </div>
          <div class="carousel-item ">
-           <img class="d-block w-100" src="https://cdn.pixabay.com/photo/2016/02/21/17/33/solar-system-1213984_640.png" alt="Third slide">
+           <img class="d-block w-100" src="https://cdn.pixabay.com/photo/2016/07/02/12/21/wallpaper-1492818_640.jpg" alt="Third slide">
+           <div class="carousel-caption d-none d-md-block">
+              <h5>Treca</h5>
+              <p>Pr slika</p>
+           </div>
          </div>
        </div>
        <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
